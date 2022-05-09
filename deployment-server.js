7bc2911ba4e83e1f0f8f1ba04491fc08312b52ec
suthyscott <sutherlandscott21@gmail.com>
2022-05-09 12:23:52 -0600

    horizontal overflow

--- deployment-server.js
+++ deployment-server.js
@@ -1,5 +1,7 @@
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use('/static', express.static(path.join(__dirname, 'public'))) //--- to serve files statically
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully.')
